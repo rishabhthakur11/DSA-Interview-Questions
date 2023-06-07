167e2e7fcc9ab3cd362dd3048cde951536e508ed
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2023-06-07 19:23:49 +0530

    Assignemnt 7 done

---
+++ Assignment-7/q4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        int i,j=0;
+        int n =s.size();
+        while(i<n)
+        {
+            while(j<n && s[j]!=' ')
+                j++;
+            
+            reverse(s.begin()+i,s.begin()+j);
+            
+            i=j+1;
+            j=i;
+            
+        }
+        
+        return s;
+        
+    }
+};
